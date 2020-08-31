528f4883a5c01a3deca0ffb78583db116f692b90
David Blyth <davidcooperblyth@gmail.com>
2020-08-31 09:45:09 -0700

    feat(core): create and pass context.Context

--- core/context.go
+++ core/context.go
@@ -199,7 +199,9 @@ func newContext(appId string) *Context {
 		LogAccumulator:      nil,
 		LogAccumulatorLevel: ANYWARN,
 		LogHook:             nil,
-		PointHook:           nil}
+		PointHook:           nil,
+		Context:             context.Background(),
+	}
 	ctx.init()
 	// We really don't like gratuitous dots in properties because
 	// they cause unncessary trouble in downstream processing.
@@ -227,6 +229,7 @@ func (ctx *Context) SubContext() *Context {
 		privilege:           ctx.privilege,
 		Context:             ctx.Context,
 		Tracer:              ctx.Tracer,
+		Context:             ctx.Context,
 	}
 
 	sub.SetLoc(ctx.GetLoc())
